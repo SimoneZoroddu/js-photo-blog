ca317bd8e951e26fac0675d56e306c4b7c629c57
SimoneZoroddu <lenviur@gmail.com>
2026-02-25 11:49:58 +0100

    milestone 3 adding card dinamic

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+const rowEl = document.querySelector('.row')
+console.log(rowEl);
 
 
 
@@ -8,8 +10,20 @@ fetch(endpoint)
         console.log(datas);
         datas.forEach(data => {
             // console.log(data.url);
-            const {title, date, url} = data
+            const { title, date, url } = data
             // console.log(title, date, url);
-            
+            const markup = `
+            <div class="col">
+                <div class="card p-2">
+                    <img src="./assets/img/pin.svg" alt="" class="pin">
+                    <img src="${url}" class="card-img-top img-fluid" alt="">
+                    <div class="card-body">
+                        <h2 class="fonts_edu">${title}</h2>
+                        <p class="card-text fonts_Sometype_Mono">${date}</p>
+                    </div>
+                </div>
+            </div>
+            `
+            rowEl.insertAdjacentHTML('beforeend', markup)
         })
     })
