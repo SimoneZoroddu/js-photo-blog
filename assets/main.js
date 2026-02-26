c2871e8e870a26d086cc813a7b88638d3a1fab0d
SimoneZoroddu <lenviur@gmail.com>
2026-02-26 15:51:33 +0100

    last commit with adjust and bonus

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,14 @@
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
-const rowEl = document.querySelector('.row')
-console.log(rowEl);
-
+const row1El = document.querySelector('#row1')
 const overlayOffEl = document.querySelector('#overlayOff')
-//console.log(overlayOffEl, overlayOnEl);
 const overlayEl = document.querySelector('#overlay')
 const imagCardEl = document.querySelector('#imagCard')
+//console.log(row1El);
+//console.log(overlayOffEl, overlayOnEl);
+
 overlayOffEl.addEventListener('click', function(){
     overlayEl.style.display = "none"
 })
-
 
 fetch(endpoint)
 .then(res => res.json())
@@ -22,26 +21,27 @@ fetch(endpoint)
         // console.log(title, date, url);
         const markup = `
         <div class="col">
-        <div class="card p-2">
-        <img src="./assets/img/pin.svg" alt="" class="pin">
-        <img src="${url}" class="card-img-top img-fluid p-2 pb-0 pt-3" alt="">
-        <div class="card-body">
-        <p class="card-text mb-0 fonts_Sometype_Mono">${date}</p>
-        <h2 class="fonts_edu fw-bold">${title}</h2>
-        </div>
-        </div>
+            <div class="card p-2">
+                <img src="./assets/img/pin.svg" alt="Foto Pin" class="pin">
+                <img src="${url}" class="card-img-top img-fluid" alt="${title}">
+                <div class="card-body">
+                    <p class="card-text mb-0 fonts_Sometype_Mono">${date}</p>
+                    <h2 class="fonts_edu fw-bold">${title}</h2>
+                </div>
+            </div>
         </div>
         `
-        rowEl.insertAdjacentHTML('beforeend', markup)
+        row1El.insertAdjacentHTML('beforeend', markup)
 
         const cardImgsTopEl = document.querySelectorAll('.card-img-top')
-        console.log(cardImgsTopEl);
+        //console.log(cardImgsTopEl);
     
         for (let i = 0; i < cardImgsTopEl.length; i++) {
             const cardimg = cardImgsTopEl[i];
             cardimg.addEventListener('click', function(){
              overlayEl.style.display = "block"
              imagCardEl.src = cardimg.src
+             imagCardEl.alt = cardimg.alt
             })
         } 
     })
