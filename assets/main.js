2779fff8d1e5634f4184b851ab1ae06000d283b9
SimoneZoroddu <lenviur@gmail.com>
2026-02-26 11:45:22 +0100

    milestone 2 realy row

--- assets/main.js
+++ assets/main.js
@@ -5,27 +5,57 @@ console.log(rowEl);
 
 
 fetch(endpoint)
-    .then(res => res.json())
-    .then(datas => {
-        console.log(datas);
-        datas.forEach(data => {
-            // console.log(data.url);
-            const { title, date, url } = data
-            // console.log(title, date, url);
-            const markup = `
-            <div class="col">
-                <div class="card p-2">
-                    <img src="./assets/img/pin.svg" alt="" class="pin">
-                    <img src="${url}" class="card-img-top img-fluid p-2 pb-0 pt-3" alt="">
-                    <div class="card-body">
-                    <p class="card-text mb-0 fonts_Sometype_Mono">${date}</p>
-                        <h2 class="fonts_edu fw-bold">${title}</h2>
-                    </div>
-                </div>
-            </div>
-            `
-            rowEl.insertAdjacentHTML('beforeend', markup)
-        })
+.then(res => res.json())
+.then(datas => {
+    console.log(datas);
+    datas.forEach(data => {
+        // console.log(data.url);
+        const { title, date, url } = data
+        // console.log(title, date, url);
+        const markup = `
+        <div class="col">
+        <div class="card p-2">
+        <img src="./assets/img/pin.svg" alt="" class="pin">
+        <img src="${url}" class="card-img-top img-fluid p-2 pb-0 pt-3" alt="">
+        <div class="card-body">
+        <p class="card-text mb-0 fonts_Sometype_Mono">${date}</p>
+        <h2 class="fonts_edu fw-bold">${title}</h2>
+        </div>
+        </div>
+        </div>
+        `
+        rowEl.insertAdjacentHTML('beforeend', markup)
     })
+    console.log(rowEl);
+    
+    const cardImgEl = document.getElementsByClassName('card-img-top')
+    //console.log(cardImgEl);
+    for (let i = 0; i < cardImgEl.length; i++) {
+        const element = cardImgEl[i];
+        //console.log(element);
+        element.addEventListener('click', function(){
+            overlayEl.style.display = "block"
+        })
+    }
+    
+})
+
+
+
+
+
+
+const overlayOnEl = document.querySelector('#overlayOn')
+const overlayOffEl = document.querySelector('#overlayOff')
+console.log(overlayOffEl, overlayOnEl);
+const overlayEl = document.querySelector('#overlay')
+
+
+overlayOnEl.addEventListener('click', function(){
+    overlayEl.style.display = "block"
+})
+overlayOffEl.addEventListener('click', function(){
+    overlayEl.style.display = "none"
+})
 
 
